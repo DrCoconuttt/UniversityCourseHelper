462e88e6d4f28af4a6bbd02ad490bf7160cd3ef6
DrCoconuttt <richardharder100@gmail.com>
2024-04-27 05:06:33 -0600

    Testing cause of listDisplay error

--- client/src/components/ListDiplay.js
+++ client/src/components/ListDiplay.js
@@ -16,41 +16,21 @@ const ListDisplay = ({list,type,search}) => {
     else if (type === "professor"){
         return (
             <div className = "listDisplay">
-                {list.map((val) => {
-                    return(
-                        <div key={val.Prof_name} value={val}>
-                            {val.Prof_name.toLowerCase().includes(search.toString().toLowerCase())? 
-                            <Link to={`/professors/${val.Prof_name}`} style={{ textDecoration: 'none' }}>{val.Prof_name}</Link> : ""}
-                        </div>
-                    );
-                })}
+
             </div>
         )
     }
     else if (type === "degree"){
         return (
             <div className = "listDisplay">
-                {list.map((val) => {
-                    return(
-                        <div key={val.Degree_name} value={val}>
-                            {val.Degree_name.toLowerCase().includes(search.toString().toLowerCase())? 
-                            <Link to={`/degrees/${val.Degree_name}`} style={{ textDecoration: 'none' }}>{val.Degree_name}</Link> : ""}
-                        </div>
-                    );
-                })}
+
             </div>
         )
     }
     else if (type === "report"){
         return (
             <div className = "listDisplay">
-                {list.map((val) => {
-                    return(
-                        <div key={val.Report_id} value={val}>
-                            <Link to={`/reports/${val.Report_id}`} style={{ textDecoration: 'none' }}>Report {val.Report_id}</Link>
-                        </div>
-                    )
-                })}
+
             </div>
         )
     }
